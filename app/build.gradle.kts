36f0999b33028c9fe9165267323d69e53b42b3a5
hyosikchoi <huchoi24@naver.com>
2025-03-13 01:15:32 +0900

    Add - Navcontroller and navigate serializable object

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -28,11 +29,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -56,4 +57,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // serializable
+    implementation(libs.kotlinx.serialization.json)
 }
--- app/src/main/java/com/hyosik/composenavigation/MainActivity.kt
+++ app/src/main/java/com/hyosik/composenavigation/MainActivity.kt
@@ -4,14 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.hyosik.composenavigation.ui.theme.ComposeNavigationTheme
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Profile(val name: String)
+
+@Serializable
+object FriendsList
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +35,90 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeNavigationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        MyApp()
+                    }
+
                 }
             }
         }
     }
 }
 
+// Define the MyApp composable, including the `NavController` and `NavHost`.
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = Profile(name = "John Smith")) {
+        composable<Profile> { backStackEntry ->
+            val profile: Profile = backStackEntry.toRoute()
+            ProfileScreen(
+                profile = profile,
+                onNavigateToFriendsList = {
+                    navController.navigate(route = FriendsList)
+                }
+            )
+        }
+        composable<FriendsList> {
+            FriendsListScreen(
+                onNavigateToProfile = {
+                    navController.navigate(
+                        route = Profile(name = "Aisha Devi")
+                    )
+                }
+            )
+        }
+    }
 }
+
+@Composable
+fun ProfileScreen(
+    profile: Profile,
+    onNavigateToFriendsList: () -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Profile for ${profile.name}")
+        Button(onClick = { onNavigateToFriendsList() }) {
+            Text("Go to Friends List")
+        }
+
+    }
+}
+
+// Define the FriendsListScreen composable.
+@Composable
+fun FriendsListScreen(onNavigateToProfile: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Friends List")
+        Button(onClick = { onNavigateToProfile() }) {
+            Text("Go to Profile")
+        }
+    }
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun NavigationPreview() {
     ComposeNavigationTheme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            MyApp()
+        }
     }
 }
